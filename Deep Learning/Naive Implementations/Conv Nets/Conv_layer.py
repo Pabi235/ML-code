0a03ea8b7a71e337fa621d3355e7bdeb7480e3e4
Pabi235 <pabbzzo@gmail.com>
2018-04-11 16:08:20 +0200

    Update Conv_layer.py

--- Deep Learning/Naive Implementations/Conv Nets/Conv_layer.py
+++ Deep Learning/Naive Implementations/Conv Nets/Conv_layer.py
@@ -35,14 +35,18 @@ class Data(object):
         self.data_mtx[depth,i,j]=val
         pass
 
-    def get_gradient(self):
-        pass
-    def set_gradient(self):
-        pass
+    def get_gradient(self,x,y,depth):
+        return self.delta_data_mtx[depth,x,y]
+
+    def set_gradient(self,x,y,depth,grad_val):
+        self.delta_data_mtx[depth,x,y] = grad_val
+
+    def add_gradient(self,x,y,depth,grad_val):
+        self.delta_data_mtx += grad_val
 
     def generate_normal_rand(self,matx_elmt):
         n_weight_vals=self.height * self.depth * self.depth
-        return matx_elmt + np.random.normal(loc=0,scale=np.sqrt(n_weight_vals),size=1)
+        return np.float(matx_elmt + np.random.normal(loc=0,scale=np.sqrt(n_weight_vals),size=1))
 
 
 class Naive_Conv_NeuralNet_Layer(object):
@@ -52,6 +56,9 @@ class Naive_Conv_NeuralNet_Layer(object):
     Details of the mathematics can be found at the from  textbook 'Deep Learning' by Goodman, Bengio et al.
     Implementation based also on stanford convolutional networks course work:
     http://cs231n.github.io/convolutional-networks/#conv
+    Code is inspired by karpathy's javascript implementation of a different deep learning layers. Its amazing. So check it out.
+    All(most since I looked at alot of different code in coming to try understand this stuff) goes to ya. Thanks for making your code public dude
+    https://github.com/karpathy/convnetjs
     """
 
     def __init__(self,input_volume,input_feature_map_dim,no_filters,filter_map_dim=3,stride_len=1,zero_padding=1,k=3):
@@ -70,7 +77,6 @@ class Naive_Conv_NeuralNet_Layer(object):
         self.filter_size =filter_map_dim
         self.stride_len=stride_len
         self.zero_padding=zero_padding
-        self.filter_vol = Data(width=self.filter_size,height=self.filter_size,depth=self.input_vol.depth)
         self.filter_map = [Data(width=self.filter_size,height=self.filter_size,depth=self.input_vol.depth) for i in range(self.n_filters)]
         self.bias_vol= Data(width=self.filter_size,height=self.filter_size,depth=1)
 
@@ -82,7 +88,7 @@ class Naive_Conv_NeuralNet_Layer(object):
         self.Output_Height=(self.input_vol.height-self.filter_size+2*self.zero_padding)/(self.stride_len+1)
         self.output_Tensor = Data(width=self.Output_Width,height=self.Output_Height,depth=self.n_filters)
 
-    def Naive_Conv_forwardpass(self):
+    def Naive_forwardpass(self):
         """
         Forward pass of conv net implementing output map generating function.
         Returns an tensor of self.Output_Width X self.Output_Height X self.n_filters. Which is happily sent off to the
@@ -97,26 +103,33 @@ class Naive_Conv_NeuralNet_Layer(object):
             for wdth_indx in range(0,self.Output_Width):
                 for hgt_indx in range(0,self.Output_Height):
 
-                    img_area=self.input_vol.data_mtx[:,wdth_indx:(wdth_indx+self.stride_len),hgt_indx:(hgt_indx+self.stride_len)]
-                    trn_img_col=self.im2col(img_area)
+                    trn_img_area=self.input_vol.data_mtx[:,wdth_indx:(wdth_indx+self.stride_len),hgt_indx:(hgt_indx+self.stride_len)]
+                    trn_img_col=self.im2col(trn_img_area)
                     self.output_Tensor.data_mtx[filter_k,hgt_indx,wdth_indx] = self.convolution_op(trn_img_col,filter_col)+self.bias_vol
         return self.output_Tensor
 
-    def Naive_Conv_backwardpass(self,X):
+    def Naive_backwardpass(self,X):
         """
         :param X:
         :return:
         """
-        #  this fucking backward pass ...sigh
+        # this fucking backward pass ...sigh
         # need to make two backward pass gradient calculations
         # gradient w.r.t filter weights which will be used for weight updates.
         # gradient w.r.t current image representation/ current layer which will be used as gradient flow to lower layers
 
         for filter_j in range(0,len(self.filter_map)):
-            filter_vol=self.filter_map[filter_j]
-            
-
-
+            filter_vol=self.filter_map[filter_j]                           # fix a single filter
+            for height_indx in range(start=0,stop=self.Output_Height):
+                for width_indx in range(start=0,stop=self.Output_Width): # fixes a single pixel , pixel i,j in layer L the output vol
+                    upstream_grad = self.input_vol.get_gradient(filter_j,width_indx,height_indx) # get dE/d(x_{i,j}) . derivative of error w.r.t fixed pixel
+                    for filter_depth_indx in range(0,filter_vol.depth):
+                        for filter_height_indx in range(0,filter_vol.height):
+                            for filter_width_indx in range(0,filter_vol.width): # for fixed w^l_{m,n,c}
+                                width_stride_dist = self.stride_len * width_indx
+                                height_stride_dist = self.stride_len * height_indx
+                                filter_vol.delta_data_mtx[filter_depth_indx,filter_width_indx,filter_height_indx] += self.input_vol.data_mtx[filter_depth_indx,width_indx+width_stride_dist,height_indx+height_stride_dist] * upstream_grad
+                                self.input_vol.delta_mtx[filter_depth_indx,width_indx+width_stride_dist,height_indx+height_stride_dist] += filter_vol.data_mtx[filter_depth_indx,filter_width_indx,filter_height_indx] * upstream_grad
 
     def im2col(self,X):
         """
@@ -133,32 +146,3 @@ class Naive_Conv_NeuralNet_Layer(object):
         conv_result=np.sum(np.dot(image_col,filter_col))
         return conv_result
 
-
-
-class ReLu(object):
-
-    def __inti__(self):
-        return ""
-    def forward_ReLU(self,X):
-
-class Loss_layer(object):
-
-
-def generate_normal_rand(matx_input,y):
-    """
-    :param matx_input: a matrix of zeros
-    :param y: variance of the neuron. Mean always assumed to be 0
-    :return:
-    """
-    return matx_input + np.random.normal(loc=0,scale=np.sqrt(y),size=1)
-
-generate_normal_rand(0,4)
-
-data_mtx= np.reshape(np.zeros(300),newshape=(3,10,10))
-def vectorized_rand_norms():
-    np.vectorize(generate_normal_rand,otypes=[np.float])
-
-vectorized_rand_norm=np.vectorize(generate_normal_rand,otypes=[np.float])
-p=vectorized_rand_norm(data_mtx,4)
-
-#p=vectorized_rand_norm(data_mtx)
