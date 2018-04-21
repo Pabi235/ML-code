e5b3b83cf4eb6421bd9d53334f8c37836cd1d2fd
Pabi235 <pabbzzo@gmail.com>
2018-04-21 15:51:22 +0200

    Update Conv_layer.py

--- Deep Learning/Naive Implementations/Conv Nets/Conv_layer.py
+++ Deep Learning/Naive Implementations/Conv Nets/Conv_layer.py
@@ -182,26 +182,18 @@ class Naive_Conv_NeuralNet_Layer(object):
         # gradient w.r.t current image representation/ current layer which will be used as gradient flow to lower layers
 
         for filter_j in range(0, len(self.filter_map)):
-            filter_vol = self.filter_map[
-                filter_j]  # fix a single filter,reference to a class object. Make sure that changes are inplace and not new copy object
+            filter_vol = self.filter_map[ filter_j]  # fix a single filter,reference to a class object. Make sure that changes are inplace and not new copy object
             for height_indx in range(start=0, stop=self.Output_Height):
-                for width_indx in range(start=0,
-                                        stop=self.Output_Width):  # fixes a single pixel , pixel i,j in layer L the output vol
+                for width_indx in range(start=0,stop=self.Output_Width):  # fixes a single pixel , pixel i,j in layer L the output vol
                     upstream_grad = np.full(shape=(filter_vol.depth, self.filter_size, self.filter_size),
                                             fill_value=self.output_Tensor.delta_data_mtx[
                                                 filter_j, width_indx, height_indx])  # get dE/d(x_{i,j}) . derivative of error w.r.t fixed pixel
-                    #                    for filter_depth_indx in range(0,filter_vol.depth):
-                    for filter_height_indx in range(0, filter_vol.height):
-                        for filter_width_indx in range(0, filter_vol.width):
-                            width_stride_dist = self.stride_len * width_indx
-                            height_stride_dist = self.stride_len * height_indx
-                            filter_vol.delta_data_mtx[:, filter_width_indx, filter_height_indx] += self.input_vol.data_mtx[
-                                                                                                   :,
-                                                                                                   width_indx + width_stride_dist,
-                                                                                                   height_indx + height_stride_dist] * upstream_grad
-                            self.input_vol.delta_mtx[:, width_indx + width_stride_dist,
-                            height_indx + height_stride_dist] += filter_vol.data_mtx[:, filter_width_indx,
-                                                                 filter_height_indx] * upstream_grad
+                    w,h,s,f = width_indx,height_indx,self.stride_len,self.filter_size
+                    filter_vol.delta_data_mtx[:, :, :] += self.input_vol.data_mtx[:,w * s:w * s + f,h * s: h * s + f]\
+                                                          * upstream_grad  # element wise multiplication
+                    self.input_vol.delta_mtx[:,w * s:w * s + f,h * s: h * s + f] += filter_vol.data_mtx[:, :,:] * upstream_grad
+
+
 
 
                             # initial try at backprop changed with something else
